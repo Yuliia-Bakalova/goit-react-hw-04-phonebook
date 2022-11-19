3869661d26155e0ea018c97c75e32d0fb3975bc9
Yuliia-Bakalova <98843051+Yuliia-Bakalova@users.noreply.github.com>
2022-11-19 16:58:49 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,16 +9,16 @@ import { LS_KEY_CONTACTS } from 'constans/ConstansKey';
 import { localStorageGetItem, localStorageSetItem } from 'utils/useLocalStorage';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return (
+  const [contacts, setContacts] = useState(
+   
       localStorageGetItem(LS_KEY_CONTACTS) ?? [
         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
         { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
       ]
-    );
-  });
+   
+  );
 
   const [filter, setFilter] = useState('');
 
